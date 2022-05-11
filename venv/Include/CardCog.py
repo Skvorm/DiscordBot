c53bd1069fb930b50a5e1b463b06c3d3edc2f7ab
mwskv <mwskvor@gmail.com>
2022-05-11 18:47:17 -0500

    -fixed !help formatting

--- venv/Include/CardCog.py
+++ venv/Include/CardCog.py
@@ -10,10 +10,10 @@ class Card(commands.Cog, name="Card"):
         self.client = client
     card_desc="draws cards"
     card_help_brief="draws cards for all users in voice channel"
-    card_help_long="Draws a card for each user currently in the same voice channel as the sending user.\
-     The card with the highest rank is the winner(Very Exciting, I know)\
-    If not connected to a voice channel, draws a single card for the user and outputs value into the text\
-     channel where the command was sent from"
+    card_help_long="Draws a card for each user currently in the same voice channel as the sending user.The card with " \
+                   "the highest rank is the winner(Very Exciting, I know)If not connected to a voice channel, " \
+                   "draws a single card for the user and outputs value into the text channel where the command was " \
+                   "sent from "
 
     @commands.command(name="card",description=card_desc,help=card_help_long,brief=card_help_brief)
     async def card(self, ctx):
