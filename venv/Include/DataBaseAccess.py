bf936504b96dcbb176c11a35bc95c5423070c876
mwskv <mwskvor@gmail.com>
2022-06-24 16:28:47 -0500

    setting up Database to Track user scores for Games/Predictions

--- venv/Include/DataBaseAccess.py
+++ venv/Include/DataBaseAccess.py
@@ -1,19 +1,34 @@
 import os
-import mysql.connector
-from dotenv import load_dotenv
+import sqlite3
 
 
 class DataBaseAccess:
-    load_dotenv()
-    host = os.getenv('DB_HOST')
-    user = os.getenv('DB_USER')
-    password = os.getenv('DB_PASS')
-    database = os.getenv('DB_NAME')
-    user_database = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    user_cursor = user_database.cursor()
+    cur = ""
 
+    count = 0
+
+    # get user
+    # put user
+    # get user_score
+    # put user_score (increment,set)
+    def __init__(self):
+        self.count = 2
+        try:
+            con = sqlite3.connect('BOTDB.db')
+            self.cur = con.cursor()
+        except Exception:
+            print("Couldn't connect to DataBase")
+        tables = self.cur.execute('''SELECT name FROM sqlite_master WHERE type='table'
+                              AND name='users';''').fetchall()
+        if not tables:
+            self.cur.execute('''CREATE TABLE users (id text, score integer, admin_point real))''')
+        else:
+            print("tables exist")
+
+    def get_count(self):
+        return self.count
+
+
+if __name__ == '__main__':
+    d = DataBaseAccess()
+    print(d.get_count())
