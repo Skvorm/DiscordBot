4181956f3a0e386d93df516232d02dba2a844eaa
mwskv <mwskvor@gmail.com>
2022-07-11 16:18:45 -0500

    text formatting

--- venv/Include/BotHelperFunctions.py
+++ venv/Include/BotHelperFunctions.py
@@ -59,7 +59,7 @@ def get_roll_range(input_string, max_val=10000, max_die=1000):
     # parses input from !roll command
     # outputs list of[min,max,# of die]
     end = 100
-    out=[]
+    out = []
     num_die = 1
     param = input_string.replace('!roll', '')
     param = param.strip()
@@ -77,17 +77,17 @@ def get_roll_range(input_string, max_val=10000, max_die=1000):
         try:
             end = int(param[1])
             num_die = int(param[0])
-            #print(f'start{start}:End{end}:NumberDice{num_die}')
+            # print(f'start{start}:End{end}:NumberDice{num_die}')
         except ValueError:
-            #print("Value ERROR")
+            # print("Value ERROR")
             end = 6
             num_die = 1
         if end <= 0:
-                end = 6
+            end = 6
         elif num_die > max_die:
-                num_die = max_die
+            num_die = max_die
         elif num_die <= 0:
-                num_die = 1
+            num_die = 1
         else:
             end = int(param[1])
         if end >= max_val:
@@ -95,76 +95,76 @@ def get_roll_range(input_string, max_val=10000, max_die=1000):
         elif end <= (max_val * -1):
             end = (-1 * max_val)
         out = [start, end, num_die]
-        print(f'{out}:A')
+        # print(f'{out}:A')
     elif param.find('-') != -1:
-        param=param.replace(' ', '')
-        ct=param.count('-')
-        #print(f'{ct}:{param}')
-        if ct==1:
-            if param.startswith('-'):
-                param=param[1:]
-                param = param.split('-')
-                start = -1*int(param[0])
-                end=0
-                #print(f'A{start},{end},{param}')
-            else:
-                param = param.split('-')
-                start = int(param[0])
-                end = int(param[1])
-                #print(f'B{start},{end},{param}')
-        elif ct==2:
+        param = param.replace(' ', '')
+        ct = param.count('-')
+        # print(f'{ct}:{param}')
+        if ct == 1:
             if param.startswith('-'):
                 param = param[1:]
                 param = param.split('-')
-                start = -1*(int(param[0]))
-                end = int(param[1])
-                #print(f'C{start},{end},{param}')
+                start = -1 * int(param[0])
+                end = 0
+                # print(f'A{start},{end},{param}')
             else:
                 param = param.split('-')
                 start = int(param[0])
-                end = -1*int(param[1])
-                #print(f'D{start},{end},{param}')
-        elif ct==3:
+                end = int(param[1])
+                # print(f'B{start},{end},{param}')
+        elif ct == 2:
+            if param.startswith('-'):
+                param = param[1:]
+                param = param.split('-')
+                start = -1 * (int(param[0]))
+                end = int(param[1])
+                # print(f'C{start},{end},{param}')
+            else:
+                param = param.split('-')
+                start = int(param[0])
+                end = -1 * int(param[1])
+                # print(f'D{start},{end},{param}')
+        elif ct == 3:
             param = param.split('-')
-            #print(param)
-            param=list(filter(None, param))
-            #print(param)
+            # print(param)
+            param = list(filter(None, param))
+            # print(param)
 
-            start =-1* int(param[0])
+            start = -1 * int(param[0])
             end = -1 * int(param[1])
-            #print(f'E{start},{end},{param}')
+            # print(f'E{start},{end},{param}')
 
         else:
             param = param.split('-')
             start = int(param[0])
             end = int(param[1])
-            #print(f'F{start},{end},{param}')
-        if end<start:
-            tmp=start
-            start=end
-            end=tmp
+            # print(f'F{start},{end},{param}')
+        if end < start:
+            tmp = start
+            start = end
+            end = tmp
         out = [start, end, 1]
     else:
-        #print(f'Default {1},{end},{param}')
-        try :
-            end=int(param)
+        # print(f'Default {1},{end},{param}')
+        try:
+            end = int(param)
             if end >= max_val:
                 end = max_val
             elif end <= (max_val * -1):
                 end = (-1 * max_val)
-            if end==0:
-                out=[0,1,1]
-            elif end==1:
-                out=[0,end,1]
-            elif end>1:
-                out=[1,end,1]
-            elif end<0:
-                out=[end,-1,1]
+            if end == 0:
+                out = [0, 1, 1]
+            elif end == 1:
+                out = [0, end, 1]
+            elif end > 1:
+                out = [1, end, 1]
+            elif end < 0:
+                out = [end, -1, 1]
         except ValueError:
             out = [1, 100, 1]
         # min,max,times rolled
-        #out = [1, 100, 1]
-    #print(out)
+        # out = [1, 100, 1]
+    # print(out)
     return out
 
 
@@ -198,7 +198,8 @@ def parse_song(input_string):
         param = -1
     return param
 
-#returns flat list of all playable media files
+
+# returns flat list of all playable media files
 def create_music_list(path):
     music_list = []
     dir_list = []
@@ -216,8 +217,9 @@ def create_music_list(path):
     c.close()
     return music_list
 
-#returns dictionary of media files
-#folder:[list of playable files in folder]
+
+# returns dictionary of media files
+# folder:[list of playable files in folder]
 def create_music_list_dir(path):
     music_list = []
     dir_list = {}
@@ -226,15 +228,13 @@ def create_music_list_dir(path):
         if f.is_dir():
             tmp_list = create_music_list(f.path)
             for tf in tmp_list:
-                dir_list[f.path]=tmp_list
+                dir_list[f.path] = tmp_list
         elif f.is_file():
             if filetype.is_audio(f.path):
                 music_list.append(f.path)
-    dir_list[path]=music_list
+    dir_list[path] = music_list
     c.close()
     return dir_list
-
-
 
 
 class BotHelperFunctions:
@@ -243,9 +243,9 @@ class BotHelperFunctions:
 
 if __name__ == '__main__':
     d = BotHelperFunctions()
-  #  tmp = parse_song("!music 9")
-  #  tmp = parse_song("!music")
- #   print(str(tmp) + ":" + str(parse_song("!music")))
-  # tmp=create_music_list_dir("music")
-   ####       print(f'{song_format(song)}')
+    #  tmp = parse_song("!music 9")
+    #  tmp = parse_song("!music")
+    #   print(str(tmp) + ":" + str(parse_song("!music")))
+    # tmp=create_music_list_dir("music")
+    ####       print(f'{song_format(song)}')
     print(get_roll_range("3d20"))
